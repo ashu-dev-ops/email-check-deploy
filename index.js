1d98e8bc800cfa3c083164da852c1fbf1c90def7
ashu-dev-ops <computerlogic25@gmail.com>
2024-01-02 14:18:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const emailValidator = require("deep-email-validator");
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", async (req, res) => {
+  res.send("Hello World!");
+  // console.log("running");
+  //   try {
+  //     const data = await emailValidator.validate(req.body.email);
+  //     console.log(data);
+  //     res.json({ data });
+  //   } catch (error) {
+  //     res.status(500).json({ error });
+  //   }
+});
+app.post("/", async (req, res) => {
+  console.log("running post");
+  try {
+    console.log("running valid");
+    const data = await emailValidator.validate("computerlogic25@gmail.com");
+    console.log("data i am sending", data);
+    return res.json({ data });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
