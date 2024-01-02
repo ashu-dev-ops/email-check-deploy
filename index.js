131038551ec479c4d201305db3aa44129c19dc6e
ashu-dev-ops <computerlogic25@gmail.com>
2024-01-02 14:25:33 +0530

    n

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ const app = express();
 const port = 3000;
 
 app.get("/", async (req, res) => {
-  res.send("Hello World!");
-  // console.log("running");
-  //   try {
-  //     const data = await emailValidator.validate(req.body.email);
-  //     console.log(data);
-  //     res.json({ data });
-  //   } catch (error) {
-  //     res.status(500).json({ error });
-  //   }
+//   res.send("Hello World!");
+try {
+    console.log("running valid");
+    const data = await emailValidator.validate("computerlogic25@gmail.com");
+    console.log("data i am sending", data);
+    return res.json({ data });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error });
+  }
 });
 app.post("/", async (req, res) => {
   console.log("running post");
